595d3bafb161086061f3be95c6de8c9bba643c81
nihil <nihilbhi@swbell.net>
2017-07-10 20:15:52 -0500

    simplify UNet

--- piwise-master/piwise/network.py
+++ piwise-master/piwise/network.py
@@ -154,23 +154,22 @@ class UNetDec(nn.Module):
 class UNet(nn.Module):
 
     def __init__(self, num_classes):
-        super().__init__()
 
-        self.dec1 = UNetDec(3, 64)
-        self.dec2 = UNetDec(64, 128)
-        self.dec3 = UNetDec(128, 256)
-        self.dec4 = UNetDec(256, 512, dropout=True)
+        super().__init__()
+        self.dec1 = UNetDec(2, 64)
+        self.dec2 = UNetDec(64, 128, dropout=True)
         self.center = nn.Sequential(
-            nn.Conv2d(512, 1024, 3),
+            nn.Conv2d(128, 256, 3),
             nn.ReLU(inplace=True),
-            nn.Conv2d(1024, 1024, 3),
+            nn.Conv2d(256, 256, 3),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.ConvTranspose2d(1024, 512, 2, stride=2),
+            nn.ConvTranspose2d(256, 128, 2, stride=2),
             nn.ReLU(inplace=True),
         )
-        self.enc4 = UNetEnc(1024, 512, 256)
-        self.enc3 = UNetEnc(512, 256, 128)
+
+        #self.enc4 = UNetEnc(1024, 512, 256)
+        #self.enc3 = UNetEnc(512, 256, 128)
         self.enc2 = UNetEnc(256, 128, 64)
         self.enc1 = nn.Sequential(
             nn.Conv2d(128, 64, 3),
@@ -183,19 +182,19 @@ class UNet(nn.Module):
     def forward(self, x):
         dec1 = self.dec1(x)
         dec2 = self.dec2(dec1)
-        dec3 = self.dec3(dec2)
-        dec4 = self.dec4(dec3)
-        center = self.center(dec4)
-        enc4 = self.enc4(torch.cat([
-            center, F.upsample_bilinear(dec4, center.size()[2:])], 1))
-        enc3 = self.enc3(torch.cat([
-            enc4, F.upsample_bilinear(dec3, enc4.size()[2:])], 1))
+        #dec3 = self.dec3(dec2)
+        #dec4 = self.dec4(dec3)
+        center = self.center(dec2)
+        #enc4 = self.enc4(torch.cat([
+            #center, F.upsample_bilinear(dec4, center.size()[2:])], 1))
+        #enc3 = self.enc3(torch.cat([
+            #center, F.upsample_bilinear(dec3, center.size()[2:])], 1))
         enc2 = self.enc2(torch.cat([
-            enc3, F.upsample_bilinear(dec2, enc3.size()[2:])], 1))
+            center, F.upsample_bilinear(dec2, center.size()[2:])], 1))
         enc1 = self.enc1(torch.cat([
             enc2, F.upsample_bilinear(dec1, enc2.size()[2:])], 1))
 
-        return F.upsample_bilinear(self.final(enc1), x.size()[2:])
+	return F.upsample_bilinear(self.final(enc1), x.size()[2:])
 
 
 class SegNetEnc(nn.Module):
